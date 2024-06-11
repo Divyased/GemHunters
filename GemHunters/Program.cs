e8bc9622d664425d79e3250e68118efa2e6354a9
Divya Sirling Edward <Dsirlingedward1882@conestogac.on.ca>
2024-06-11 01:22:29 -0400

    A new class "Board" has been added with initializing board, and objects(gems, obstacles).

--- GemHunters/Program.cs
+++ GemHunters/Program.cs
@@ -60,4 +60,47 @@ class Cell
         Occupant = occupant;
     }
 }
+class Board
+{
+    private Cell[,] grid;
+    public Board()
+    {
+        grid= new Cell[6,6];
+        InitializeBoard();
+    }
+        private void InitializeBoard()
+    {
+        for (int i = 0; i<6; i++) 
+        { 
+            for(int j = 0; j<6; j++)
+            {
+                grid[i, j] = new Cell("-");
+            }
+        }
+        grid[0, 0].Occupant = "P1";
+        grid[5, 5].Occupant = "P2";
+        InitializingObjects("O", 5);
+        InitializingObjects("G", 15);
+    }
+    private void InitializingObjects(string obj, int count)
+    {
+        Random random = new Random();
+        int gemcnt= 0;
+        while(gemcnt<count)
+        {
+            int x= random.Next(6);
+            int y= random.Next(6);
+            if (grid[y, x].Occupant == "-")
+            {
+                grid[y,x].Occupant = obj;
+                gemcnt++;
+            }
+
+        }
+    }
+    public void Display()
+    {
+
+    }
+}
 
